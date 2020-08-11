1555a681abb7ead44491d43ce7dda04a51f9970d
zeqiang.chen <1209120868@qq.com>
2020-08-11 11:41:38 +0800

    [zeqiang] feat: add ListActivity

--- app/src/main/java/com/thoughtworks/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/thoughtworks/recyclerviewdemo/MainActivity.java
@@ -2,13 +2,31 @@ package com.thoughtworks.recyclerviewdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button recyclerViewVButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewVButton = findViewById(R.id.recycler_view_button);
+        recyclerViewVButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openListActivity();
+            }
+        });
+
+    }
+
+    private void openListActivity() {
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
     }
 }
